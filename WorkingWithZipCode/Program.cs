1d4d0a99385ae5f55f6982c59c2dd4ab327ba2f5
Henrique E.Gregorio <henriquee.gr@gmail.com>
2023-01-02 20:24:25 -0300

    trabanhando com via cep

---
+++ WorkingWithZipCode/Program.cs
@@ -0,0 +1,23 @@
+﻿using Caelum.Stella.CSharp.Http;
+using System.Diagnostics;
+
+var zipCodes = new List<string>()
+{
+     "34580170"
+    ,"31250280"
+};
+
+foreach (var zipCode in zipCodes)
+{
+    GetAddress(zipCode);
+}
+
+static void GetAddress(string zipCode)
+{
+    var url = $"http://viacep.com.br/ws/{zipCode}/json/";
+    
+    var jsonAddress = new ViaCEP().GetEnderecoJson(zipCode);
+    Debug.WriteLine(jsonAddress);
+
+    var asyncJsonAddress = new ViaCEP().GetEnderecoJson(zipCode);
+}
